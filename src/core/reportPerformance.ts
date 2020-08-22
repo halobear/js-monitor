e0961ad3c4b48ecf0299c5cc7187abe3bf8354d9
luzhongkuan <luzhongk@126.com>
2020-08-22 14:59:36 +0800

    fix: 性能提交添加用户id和项目id

--- src/core/reportPerformance.ts
+++ src/core/reportPerformance.ts
@@ -3,8 +3,13 @@ import { stringify } from './util'
 
 // 上报性能
 function reportPerformance(options: PerformanceOptions, config: InitOptions) {
+  const { pid, uid } = config
   const img = new Image()
-  const src = `${config.reportUrl}/performance?${stringify(options)}`
+  const src = `${config.reportUrl}/performance?${stringify({
+    ...options,
+    pid,
+    uid,
+  })}`
   img.src = src
 }
 
